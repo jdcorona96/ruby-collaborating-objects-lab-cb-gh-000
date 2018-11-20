49229d86f9f897660bb417841f316af2c73d0ab1
Joseph Corona <josed.hc96@gmail.com>
2018-11-20 04:06:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,8 @@ class Song
     file_data = _filename.split(" - ")
     song = Song.new(file_data[1])
     artist = Artist.find_or_create_by_name(file_data[0])
-    #artist.add_song(song)
+    binding.pry
+    artist.add_song(song)
     song.artist = artist
     song
   end
