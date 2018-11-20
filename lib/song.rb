e2a75c2438f77ad618e9b046b4e269e7a99047a0
Joseph Corona <josed.hc96@gmail.com>
2018-11-20 02:55:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,15 @@ class Song
 
   def initialize(_name)
     @name = _name
-    
+
   end
 
   attr_accessor :name, :artist, :genre
+
+  def self.new_by_filename(_filename)
+    _filename.split(" - ")
+    song = Song.new()
+    
+    
+  end
 end #song
